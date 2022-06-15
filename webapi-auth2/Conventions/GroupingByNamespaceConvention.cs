49b9bfb230ceee151db8972128512d62b077f238
ponyboy3 <edward@theemm.com>
2022-06-15 09:07:56 -0700

    init commit prior to rename

---
+++ webapi-auth2/Conventions/GroupingByNamespaceConvention.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8602 // Dereference of a possibly null reference.
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace webapi_auth2.Conventions
+{
+    public class GroupingByNamespaceConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var controllerNamespace = controller.ControllerType.Namespace;
+            var apiVersion = controllerNamespace.Split(".").Last().ToLower();
+            if (!apiVersion.StartsWith("v")) { apiVersion = "v1"; }
+            controller.ApiExplorer.GroupName = apiVersion;
+        }
+    }
+}
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
---
+++ webapi-auth2/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var titleBase = "Parks API";
+
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(new webapi_auth2.Conventions.GroupingByNamespaceConvention());
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(config =>
+{
+    var description = "This is a Web API for Parks operations";
+    var TermsOfService = new Uri("https://udemy.com/user/evgeniyameshuris/");
+    var License = new OpenApiLicense()
+    {
+        Name = "MIT"
+    };
+
+    var Contact = new OpenApiContact()
+    {
+        Name = "Evgeniya Meshuris",
+        Email = "em@theemm.com",
+        Url = new Uri("https://evm.blog/")
+    };
+
+    config.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = titleBase + " v1",
+        Description = description,
+        TermsOfService = TermsOfService,
+        License = License,
+        Contact = Contact
+    });
+
+    config.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = titleBase + " v2",
+        Description = $"{description} and is now secured via api key.",
+        TermsOfService = TermsOfService,
+        License = License,
+        Contact = Contact
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(config =>
+    {
+        config.SwaggerEndpoint("/swagger/v1/swagger.json", titleBase + " v1");
+        config.SwaggerEndpoint("/swagger/v2/swagger.json", titleBase + " v2");
+    });
+
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
